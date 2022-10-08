e487208d2a03f92a091c9a93c4fe2d1af1140a39
Kitsune <67001745+Akai-Kitsune@users.noreply.github.com>
2022-10-08 18:48:01 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express.Router();
+
+/*
+  This 
+*/
+
+/* GET home page. */
+app.get('/market', function(req, res, next) {
+  res.send('MARKET');
+});
+
+app.get('/lk', function(req, res, next){
+  res.send("THIS IS LK")
+})
+
+app.get('/register', function(req, res, next) {
+  //res.render('sindex', '/static/register.html')
+  res.redirect('/static/register.html')
+});
+
+
+module.exports = app;
