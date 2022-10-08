feb86094b2c735b805fdf0d8f24413743249fbfb
Kitsune <67001745+Akai-Kitsune@users.noreply.github.com>
2022-10-09 00:09:18 +0300

    add
    Add database

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var app = express.Router();
-/*
-  This 
-*/
+var path = require('path');
+const fs = require('fs');
+const { dirname } = require('path');
+const urlencodedParser = express.urlencoded({extended: true});
+
+var loginFromServer = require('../bin/data/users.json')
+
+/* GET home page. */
+app.post('/', urlencodedParser, function(req, res) {
+
+  data = loginFromServer
+  
+  console.log(data)
+  console.log(data['users'])
+  console.log(data['users']['admin@vtb.ru'])
+  console.log(req.body.user_login)
+
+  if(data['users'][req.body.user_login] != undefined && data['users'][req.body.user_login]['password'] == req.body.user_password){
+    res
+      .status(12)
+      .cookie('access_token', {'rank' : 'admin', 'id' : data['users'][req.body.user_login]['id']})
+      .redirect('profile.html')
+  }
+  else{
+    res.send("WRONG LOGIN")
+  }
+});
 
 /* GET home page. */
 app.get('/market', function(req, res, next) {
